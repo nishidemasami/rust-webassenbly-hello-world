00c9d83db17c22c26c26695dc8a08ed53ec081eb
NISHIDE, Masami <nishidemasami@gmail.com>
2022-11-16 13:47:42 +0000

    fix for GitHub Codespaces

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -20,5 +20,5 @@ rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
 rustup component add clippy --toolchain nightly
 
-cargo install cargo-expand
-cargo install cargo-edit
+# cargo install cargo-expand
+# cargo install cargo-edit
